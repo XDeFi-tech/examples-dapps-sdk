8b10c5091c02a9ea58160a0b0aa59c973ef9334e
David P <davidphan33@gmail.com>
2021-02-21 16:05:41 +0200

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 yarn install
 mkdir serving_directory/
-cd binancedex/xdefi-vue-binancedex && yarn install && yarn build && mv dist/ ../../serving_directory && cd ../..
-cd bitcoin/xdefi-vue-bitcoin-sdk && yarn install && yarn build && mv dist/ ../../serving_directory && cd ../..
-cd ethereum/web3/web3-example-vue && yarn install && yarn build && mv dist/ ../../serving_directory && cd ../../..
+cd binancedex/xdefi-vue-binancedex && yarn install && yarn build && mv dist/ ../../serving_directory/binance && cd ../..
+cd bitcoin/xdefi-vue-bitcoin-sdk && yarn install && yarn build && mv dist/ ../../serving_directory/bitcoin && cd ../..
+cd ethereum/web3/web3-example-vue && yarn install && yarn build && mv dist/ ../../../serving_directory/ethereum && cd ../..
