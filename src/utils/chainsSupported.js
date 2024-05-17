b6e5d121e45dd768d5d635e05ee5b5be0433d3b2
HoangVD2 <hoang.vu2@sotatek.com>
2024-05-17 17:05:50 +0700

    base: XDEFI-6845 add missing chains example

---
+++ src/utils/chainsSupported.js
@@ -0,0 +1,168 @@
+const chainsSupported = [
+  {
+    chain: 'akash',
+    name: 'Akash',
+    baseChain: 'CosmosChain',
+    chainId: 'akashnet-2',
+  },
+  {
+    chain: 'arbitrum',
+    name: 'Arbitrum',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'aurora',
+    name: 'Aurora',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'avalanche',
+    name: 'Avalanche',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'axelar',
+    name: 'Axelar',
+    baseChain: 'CosmosChain',
+    chainId: 'axelar-dojo-1',
+  },
+  {
+    chain: 'binance',
+    name: 'Binance',
+    baseChain: 'BinanceChain',
+  },
+  {
+    chain: 'binanceSmartChain',
+    name: 'Binance Smart Chain',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'bitcoin',
+    name: 'Bitcoin',
+    UTXO: true,
+  },
+  {
+    chain: 'bitcoincash',
+    name: 'Bitcoin Cash',
+    UTXO: true,
+  },
+  {
+    chain: 'cantoEVM',
+    name: 'Canto',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'cosmos',
+    name: 'Cosmos Hub',
+    baseChain: 'CosmosChain',
+    chainId: 'cosmoshub-4',
+  },
+  {
+    chain: 'crescent',
+    name: 'Crescent',
+    baseChain: 'CosmosChain',
+    chainId: 'crescent-1',
+  },
+  {
+    chain: 'cronosEVM',
+    name: 'Cronos',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'dogecoin',
+    name: 'Dogecoin',
+    UTXO: true,
+  },
+  {
+    chain: 'ethereum',
+    name: 'Ethereum',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'fantom',
+    name: 'Fantom',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'juno',
+    name: 'Juno',
+    baseChain: 'CosmosChain',
+    chainId: 'juno-1',
+  },
+  {
+    chain: 'kava',
+    name: 'Kava',
+    baseChain: 'CosmosChain',
+    chainId: 'kava_2222-10',
+  },
+  {
+    chain: 'kujira',
+    name: 'Kujira',
+    baseChain: 'CosmosChain',
+    chainId: 'kaiyo-1',
+  },
+  {
+    chain: 'litecoin',
+    name: 'Litecoin',
+    UTXO: true,
+  },
+  {
+    chain: 'mayachain',
+    name: 'Maya Protocol',
+  },
+  {
+    chain: 'mars',
+    name: 'Mars (MARS)',
+    baseChain: 'CosmosChain',
+    chainId: 'mars-1',
+  },
+  {
+    chain: 'near',
+    name: 'NEAR',
+    baseChain: 'NEAR',
+  },
+  {
+    chain: 'optimism',
+    name: 'Optimism',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'osmosis',
+    name: 'Osmosis',
+    baseChain: 'CosmosChain',
+    chainId: 'osmosis-1',
+  },
+  {
+    chain: 'polygon',
+    name: 'Polygon',
+    baseChain: 'EVM',
+  },
+  {
+    chain: 'solana',
+    name: 'Solana',
+    baseChain: 'Solana',
+  },
+  {
+    chain: 'stargaze',
+    name: 'Stargaze',
+    baseChain: 'CosmosChain',
+    chainId: 'stargaze-1',
+  },
+  {
+    chain: 'stride',
+    name: 'Stride',
+    baseChain: 'CosmosChain',
+    chainId: 'stride-1',
+  },
+  {
+    chain: 'terra',
+    name: 'Terra',
+    baseChain: 'Terra',
+  },
+  {
+    chain: 'thorchain',
+    name: 'ThorChain',
+  },
+];
+
+export default chainsSupported;
